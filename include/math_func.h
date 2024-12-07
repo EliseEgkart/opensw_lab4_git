4a432a1cc2ac96ed526e8d62b7dd5c1de87b34cd
Kim, Hyeongjin <ky942400@gmail.com>
2024-12-07 16:29:43 +0900

    addition division_func

--- include/math_func.h
+++ include/math_func.h
@@ -3,5 +3,6 @@
 
 int add(int a, int b);
 int multiple(int a, int b);
+int division(int a, int b);
 
 #endif // MATH_FUNC_H
---
+++ src/division_func.c
@@ -0,0 +1,6 @@
+#include "math_func.h"
+
+int division(int a, int b)
+{
+    return a / b;
+}
