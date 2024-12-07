7de4af73db6d076fdc2a1a62608745f81fbe9fb2
Kim, Hyeongjin <ky942400@gmail.com>
2024-12-07 16:20:16 +0900

    addition mulitple_func!

--- include/math_func.h
+++ include/math_func.h
@@ -2,5 +2,6 @@
 #define MATH_FUNC_H
 
 int add(int a, int b);
+int multiple(int a, int b);
 
 #endif // MATH_FUNC_H
---
+++ src/multiple_func.c
@@ -0,0 +1,6 @@
+#include "math_func.h"
+
+int multiple(int a, int b)
+{
+    return a * b;
+}
