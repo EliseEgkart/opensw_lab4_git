d22da2ef11cd3be7429ead3e9da1e4bb52dc03c5
Kim, Hyeongjin <ky942400@gmail.com>
2024-12-07 16:33:32 +0900

    addition print_mathgit status

--- include/math_func.h
+++ include/math_func.h
@@ -4,5 +4,6 @@
 int add(int a, int b);
 int multiple(int a, int b);
 int minus(int a, int b);
+void print_math(void);
 
 #endif // MATH_FUNC_H
---
+++ src/print_math.c
@@ -0,0 +1,7 @@
+#include "math_func.h"
+#include <stdio.h>
+
+void print_math(void)
+{
+    printf("Math is Fun!\n");
+}
